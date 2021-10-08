a8a2d80826d00a7f1d55fa63dcb28d440aa2de78
nonsodaniel <nonsodaniel07@gmail.com>
2021-10-08 23:26:15 +0100

    Restructure  views based on current status

--- src/components/countdown/Confetti.js
+++ src/components/countdown/Confetti.js
@@ -4,9 +4,6 @@ import Confetti from "react-confetti";
 const Confettis = ({ category }) => {
   return (
     <div className="confetti">
-      <div className="scroll-text">
-        <h1 className="text">Happy {category}</h1>
-      </div>
       <Confetti numberOfPieces={1000} />;
     </div>
   );
